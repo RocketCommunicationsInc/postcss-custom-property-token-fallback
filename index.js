dac60bd06106b0b148fb112a28fccdd4b7009af3
Micah Jones <micah.jones@rocketcom.com>
2023-05-10 10:41:32 -0600

    update to postcss 8

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-const postcss = require('postcss')
-
-module.exports = postcss.plugin('add-fallbacks', (options) => {
-    const index = options.index || {}
-    return (root) => {
-        root.walkDecls((decl) => {
-            const customProperties =
-                decl.value.match(/var\((--[\w-]+)(, [^)]+)?\)/g) || []
-            customProperties.forEach((property) => {
-                const [propertyName, fallbackValue] = property
-                    .replace(/var\(|\)/g, '')
-                    .split(', ')
-                const value =
-                    fallbackValue || index[propertyName.replace('--', '')] || ''
-                if (value) {
-                    decl.value = decl.value.replace(
-                        property,
-                        `var(${propertyName}, ${value})`
-                    )
-                }
-            })
-        })
-    }
-})
+// const postcss = require("postcss");
+const plugin = (options) => {
+  const index = options.index || {};
+  return {
+    postcssPlugin: "add-fallbacks",
+    Once(root) {
+      root.walkDecls((decl) => {
+        const customProperties =
+          decl.value.match(/var\((--[\w-]+)(, [^)]+)?\)/g) || [];
+        customProperties.forEach((property) => {
+          const [propertyName, fallbackValue] = property
+            .replace(/var\(|\)/g, "")
+            .split(", ");
+          const value =
+            fallbackValue || index[propertyName.replace("--", "")] || "";
+          if (value) {
+            decl.value = decl.value.replace(
+              property,
+              `var(${propertyName}, ${value})`
+            );
+          }
+        });
+      });
+    },
+  };
+};
+plugin.postcss = true;
+module.exports = plugin;
